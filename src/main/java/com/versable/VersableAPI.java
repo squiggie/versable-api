79a1d7ab1573f2bbf55d167fb0547c8216f6d01d
squiggie <squigge@gmail.com>
2016-03-28 20:33:27 -0500

    env vars

--- src/main/java/com/versable/VersableAPI.java
+++ src/main/java/com/versable/VersableAPI.java
@@ -2,11 +2,19 @@ package com.versable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class VersableAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VersableAPI.class, args);
+		Map<String, String> env = System.getenv();
+        Map<String, Object> db = new HashMap<>();
+        db.put("spring.data.mongodb.uri", env.get("DB_URI"));
+		new SpringApplicationBuilder().properties(db).sources(VersableAPI.class).run(args);
+		//SpringApplication.run(VersableAPI.class, args);
 	}
 }
